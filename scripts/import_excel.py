513e13642d6f35b1625a01f3a4ea264206208c91
Lorenzo Ferreyra <ferreyralorenzo2@gmail.com>
2024-07-15 16:40:32 -0300

    Data Import finished

--- scripts/import_excel.py
+++ scripts/import_excel.py
@@ -18,18 +18,19 @@ django.setup()
 from control_app.models import Empleado, Equipo, Actividad, Ruta
 
 # Cargar el archivo Excel
-df = pd.read_excel(r"C:\Users\Lorenzo\Downloads\Archivo_Import_Renato.xlsx",sheet_name='AUX-RUTAS')
+df = pd.read_excel(r"C:\Users\Lorenzo\Downloads\Archivo_Import_Renato.xlsx",sheet_name='AUX-ACT')
 
 # Iterar sobre las filas del DataFrame y crear instancias del modelo
 for index, row in df.iterrows():
-    Ruta.objects.create(
-        ruta=row['RUTA 2021'],
-        distrito=row['DISTRITO'],
-        zona=row['ZONA'],
-        longitud_km=row['LONG. Km 2006'],
-        tramo=row['TRAMO'],
-        lugar_inicial=row['LUGAR INICIAL'],
-        lugar_final=row['LUGAR FINAL'],
-        km_inicial='1',
-        km_final='1',
+    Actividad.objects.create(
+        codigo=row['CODIGO'],
+        grupo=row['GRUPO'],
+        actividad=row['ACTIVIDAD'],
+        subactividad=row['Subactividad'],
+        unidad=row['UNIDAD'],
+        produccion_diaria_min=row['PRODUCC DIARIA [MIN]'] if pd.notna(row['PRODUCC DIARIA [MIN]']) else 0.0,
+        produccion_diaria_max=row['PRODUCC DIARIA [MAX]'] if pd.notna(row['PRODUCC DIARIA [MAX]']) else 0.0,
+        produccion_diaria_prom=row['PRODUCC DIARIA [PROM]'] if pd.notna(row['PRODUCC DIARIA [PROM]']) else 0.0,
+        produccion_unid_hr_prom=row['PRODUCC UNID/HR [PROM]'] if pd.notna(row['PRODUCC UNID/HR [PROM]']) else 0.0,
+        produccion_hr_unid_prom=row['PRODUCC HR/UNID [PROM]'] if pd.notna(row['PRODUCC HR/UNID [PROM]']) else 0.0,
     )
